99eca26b2b465831cc5a90a266c0d8b6345c162c
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2013-03-12 12:47:25 -0500

    Presentation updates

---
+++ code/service/src/test/groovy/com/objectpartners/comics/ComicVineServiceSpec.groovy
@@ -0,0 +1,12 @@
+package com.objectpartners.comics
+ 
+import spock.lang.Specification
+class ComicVineServiceSpec extends Specification {
+   def "test something"() {
+       when:
+       true
+
+       then:
+       true
+   }
+}
